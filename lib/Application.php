006affc530966704937c55611fadb1669026b9f6
Jan Schneider <jan@horde.org>
2022-06-07 11:16:08 +0200

    [jan] Fix method call when removing user data.

--- lib/Application.php
+++ lib/Application.php
@@ -357,7 +357,7 @@ class Turba_Application extends Horde_Registry_Application
                 try {
                     $driver = $GLOBALS['injector']
                         ->getInstance('Turba_Factory_Driver')
-                        ->createFromSource($source, $sourceId);
+                        ->createFromConfig($source, $sourceId);
                 } catch (Turba_Exception $e) {
                     Horde::log($e, 'ERR');
                     continue;
