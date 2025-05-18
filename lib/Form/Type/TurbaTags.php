a2299cb63441b647f665a6656e158e1a809b8e91
Ralf Lang <ralf.lang@ralf-lang.de>
2025-05-18 20:38:12 +0200

    fix: Remove reference from signatures.

--- lib/Form/Type/TurbaTags.php
+++ lib/Form/Type/TurbaTags.php
@@ -25,12 +25,12 @@
  */
 class Turba_Form_Type_TurbaTags extends Horde_Form_Type
 {
-    public function getInfo(&$vars, &$var, &$info)
+    public function getInfo($vars, $var, $info)
     {
         $info = $var->getValue($vars);
     }
 
-    public function isValid(&$var, &$vars, $value, &$message)
+    public function isValid($var, $vars, $value, $message)
     {
         return true;
     }
